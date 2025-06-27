011a7881dcf5793b9c2e211e5b11cd6d46eb8c22
escobar25250616 <jeanherman001@gmail.com>
2025-06-27 19:11:49 +0200

    Initial commit du site banque postale

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+# Identifiants corrects
+CORRECT_ID = "0840405208"
+CORRECT_PWD = "fuckazerty"
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    erreur = None
+    if request.method == "POST":
+        user_id = request.form.get("identifiant")
+        password = request.form.get("motdepasse")
+        if user_id == CORRECT_ID and password == CORRECT_PWD:
+            return redirect("/bloque")
+        else:
+            erreur = "Identifiant ou mot de passe incorrect."
+    return render_template("login.html", erreur=erreur)
+
+@app.route("/bloque")
+def bloque():
+    return render_template("bloque.html")
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
